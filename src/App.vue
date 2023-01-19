e91c21c2fa443f1931f21438172c881b203a7e53
Gianmarco720 <orabonagianmarco379@gmail.com>
2023-01-19 18:00:23 +0100

    add ajax call

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
     components: {
-        HelloWorld,
+
+    },
+    data() {
+        return {
+            projects: null,
+            base_api_url: 'http://localhost:8000'
+        }
+    },
+    methods: {
+        getProjects(url) {
+            // Ajax call to get all projects
+            axios
+                .get(url)
+                .then(response => {
+                    console.log(response.data.results);
+                })
+                .catch(error => {
+                    console.log(error.message);
+                })
+        }
+    },
+    mounted() {
+        this.getProjects(this.base_api_url + '/api/projects');
     }
 }
 </script>
@@ -12,17 +35,10 @@ export default {
     <section class="vue-home">
         <div class="container">
             <div class="row">
-                <div class="col-12 d-flex flex-column justify-content-center align-items-center vh-100">
-                    <div class="logos">
-                        <a href="https://vitejs.dev" target="_blank">
-                            <img src="/vite.svg" class="logo" alt="Vite logo" />
-                        </a>
-                        <a href="https://vuejs.org/" target="_blank">
-                            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-                        </a>
-                    </div>
-                    <HelloWorld />
-                </div>
+                <h1>Posts</h1>
+                <!-- <div class="col-12 d-flex flex-column justify-content-center align-items-center vh-100">
+
+                </div> -->
             </div>
         </div>
     </section>
@@ -30,31 +46,4 @@ export default {
 
 <style lang="scss">
 @use './styles/general.scss';
-
-.vue-home {
-    color: #2c3e50;
-    background: #181818;
-    transition: color 0.5s, background-color 0.5s;
-    line-height: 1.6;
-    font-family: Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
-        Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
-    font-size: 15px;
-    text-rendering: optimizeLegibility;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-}
-
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-}
-
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
