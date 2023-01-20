2566a77ea93c4ab9e6a1c02ad277eb208b5327fb
Gianmarco720 <orabonagianmarco379@gmail.com>
2023-01-20 15:44:33 +0100

    add router.js

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -60,37 +60,42 @@ export default {
 <template>
     <section class="vue-home">
         <div class="container">
-            <h1>Posts</h1>
-            <div class="row row-cols-1 row-cols-sm-3 g-4">
-                <div class="col" v-for="project in projects">
-                    <!-- Project -->
-                    <div class="card border-0 shadow-sm rounded-0 rounded-bottom">
-                        <!-- Project's image or placeholder -->
-                        <img class="card-image rounded-top" :src="getImagePath(project.cover_image)" alt="">
-                        <!-- Project's text and info -->
-                        <div class="card-body">
-                            <h4>{{ project.title }}</h4>
-                            <p class="card-text" v-if="project.body">{{ trimBody(project.body) }}</p>
-                            <p class="card-text" v-else="">Sorry, this project has no description yet</p>
-                            <a href="#">Read More></a>
-                        </div>
-                        <!-- Project's category or type -->
-                        <div class="card-footer text-muted">
-                            <div class="type">
-                                <strong>Type: </strong>
-                                <span v-if="project.type">{{ project.type.name }}</span>
-                                <span v-else>Sorry, this project has no type yet</span>
+            <template v-if="projects && !loading">
+                <h1>Posts</h1>
+                <div class="row row-cols-1 row-cols-sm-3 g-4">
+                    <div class="col" v-for="project in projects">
+                        <!-- Project -->
+                        <div class="card border-0 shadow-sm rounded-0 rounded-bottom">
+                            <!-- Project's image or placeholder -->
+                            <img class="card-image rounded-top" :src="getImagePath(project.cover_image)" alt="">
+                            <!-- Project's text and info -->
+                            <div class="card-body">
+                                <h4>{{ project.title }}</h4>
+                                <p class="card-text" v-if="project.body">{{ trimBody(project.body) }}</p>
+                                <p class="card-text" v-else="">Sorry, this project has no description yet</p>
+                                <a href="#">Read More></a>
                             </div>
-                        </div>
-                        <!-- Project's technology -->
-                        <div class="technologies">
-                            <ul class="list-group list-unstyled" v-if="project.technologies.length > 0">
-                                <li v-for="technology in project.technologies"># {{ technology.name }}</li>
-                            </ul>
-                            <p v-else>Sorry there are no technologies in this project</p>
+                            <!-- Project's category or type -->
+                            <div class="card-footer text-muted">
+                                <div class="type">
+                                    <strong>Type: </strong>
+                                    <span v-if="project.type">{{ project.type.name }}</span>
+                                    <span v-else>Sorry, this project has no type yet</span>
+                                </div>
+                            </div>
+                            <!-- Project's technology -->
+                            <div class="technologies">
+                                <ul class="list-group list-unstyled" v-if="project.technologies.length > 0">
+                                    <li v-for="technology in project.technologies"># {{ technology.name }}</li>
+                                </ul>
+                                <p v-else>Sorry there are no technologies in this project</p>
+                            </div>
                         </div>
                     </div>
                 </div>
+            </template>
+            <div v-else>
+                <p> No posts here </p>
             </div>
         </div>
     </section>
