18b877d65cb1f4c4ab272d1526a17e13b0bfaa58
Gianmarco720 <orabonagianmarco379@gmail.com>
2023-01-25 14:55:10 +0100

    final commit

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -58,28 +58,42 @@ export default {
             If you want to ask me something fill the form to send me a message.
         </p>
 
-        <div class="alert alert-success" role="alert">
+        <div v-if="success" class="alert alert-success" role="alert">
             <strong>Message sent!</strong>
         </div>
 
 
         <form @submit.prevent="sendForm()">
+
+            <!-- Name input -->
             <div class="mb-3">
                 <label for="name" class="form-label">Full Name</label>
                 <input type="text" name="name" id="name" v-model="name" class="form-control" placeholder="John Doe"
                     aria-describedby="nameHelper">
+
+                <!-- Name error -->
+                <p v-for="(error, index) in errors.name">{{ error }}</p>
                 <small id="nameHelper" class="text-muted">Add your full name</small>
             </div>
+
+            <!-- Email input -->
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
                 <input type="email" name="email" id="email" v-model="email" class="form-control"
                     placeholder="john.doe@example.com" aria-describedby="emailHelper">
+
+                <!-- Email error -->
+                <p v-for="(error, index) in errors.email">{{ error }}</p>
                 <small id="emailHelper" class="text-muted">Add your email address</small>
             </div>
 
+            <!-- Message area -->
             <div class="mb-3">
                 <label for="message" class="form-label">Message</label>
                 <textarea class="form-control" name="message" id="message" v-model="message" rows="5"></textarea>
+
+                <!-- Message error -->
+                <p v-for="(error, index) in errors.message">{{ error }}</p>
             </div>
 
             <button type="submit" class="btn btn-primary" :disabled="loading">
